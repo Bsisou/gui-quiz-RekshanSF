319db5abe08d85bcd6ad4fed41e7304e1741451a
RekshanSF <161650038+RekshanSF@users.noreply.github.com>
2024-05-15 00:18:24 +0000

    start background image done
    adjusted window and image size to fit nicely

--- main.py
+++ main.py
@@ -1,44 +1,27 @@
 # Import module  
+import os
 from tkinter import *
 
 # Create object  
 root = Tk() 
 
 # Adjust size  
-root.geometry("3780x1890") 
+root.geometry("1400x700") 
 
-# Add image file 
-bg = PhotoImage(file = "start1.png") 
+#add image file 
+bg = PhotoImage(file="start1.png")
 
-# Create Canvas 
-canvas1 = Canvas( root, width = 400, 
-                 height = 400) 
+# Resize image
 
-canvas1.pack(fill = "both", expand = True) 
 
-# Display image 
-canvas1.create_image( 0, 0, image = bg,  
-                     anchor = "nw") 
+#Create Canvas
+canvas1= Canvas(root, width=1400, height=700)
+canvas1.pack(fill="both", expand=True)
 
-# Add Text 
-canvas1.create_text( 200, 250, text = "Welcome") 
+#display image 
+canvas1.create_image(0, 0, image=bg, anchor="nw")
 
-# Create Buttons 
-button1 = Button( root, text = "Exit") 
-button3 = Button( root, text = "Start") 
-button2 = Button( root, text = "Reset") 
 
-# Display Buttons 
-button1_canvas = canvas1.create_window( 100, 10,  
-                                       anchor = "nw", 
-                                       window = button1) 
-
-button2_canvas = canvas1.create_window( 100, 40, 
-                                       anchor = "nw", 
-                                       window = button2) 
-
-button3_canvas = canvas1.create_window( 100, 70, anchor = "nw", 
-                                       window = button3) 
 
 # Execute tkinter 
-root.mainloop() 
+root.mainloop()
