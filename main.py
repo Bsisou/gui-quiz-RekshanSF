b271c29fdd833eb4e9f20ea2e03eaa302bf4d01c
RekshanSF <161650038+RekshanSF@users.noreply.github.com>
2024-06-01 05:05:59 +0000

    added 1st question page window

--- main.py
+++ main.py
@@ -1,27 +1,41 @@
 # Import module  
 import os
-from tkinter import *
+import tkinter as tk
 
 # Create object  
-root = Tk() 
+root = tk.Tk() 
+
+#Creating Question page 1 
+def questionpage():
+
+          root.destroy()         
+          Q1 = tk.Tk() 
+          Q1.title("Question 1")
+          Q1.geometry("1400x700")
+          Q1.mainloop()
+
+
+#chnage window title name 
+root.title("Rekki's Global Mind Melt")
 
 # Adjust size  
 root.geometry("1400x700") 
 
 #add image file 
-bg = PhotoImage(file="start1.png")
-
-# Resize image
-
+bg = tk.PhotoImage(file="start1.png")
 
 #Create Canvas
-canvas1= Canvas(root, width=1400, height=700)
+canvas1= tk.Canvas(root, width=1400, height=700)
 canvas1.pack(fill="both", expand=True)
 
 #display image 
 canvas1.create_image(0, 0, image=bg, anchor="nw")
 
+#adding button to canvas 
+b1 = tk.Button(root, text='Start', width=40,
+          height=5, bd='10', command=questionpage)
 
+b1.place(x=450, y=450)
 
 # Execute tkinter 
 root.mainloop()
