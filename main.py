c97eb055b5488677787d32d8fd55308230635b21
RekshanSF <161650038+RekshanSF@users.noreply.github.com>
2024-07-12 06:40:00 +0000

    Fixed some minor errors
    - Changed the randomizer function to use the shuffle feature
    - Changed the location of the dictionary to be included in the main class to avoid it being a global variable

--- main.py
+++ main.py
@@ -6,24 +6,26 @@ from tkinter import *
 from PIL import Image, ImageTk
 import random
 
-# Dictionary of all my questions and answers
-
-# index 0 is the question, index 1-4 are the answers, index 5 is the correct answer written again, index 6 is the correct answer index
-q_a ={
-    1: ["The Lion is the National Animal \nof what Country?", 'Nepal', 'Sri Lanka', 'China', 'South Korea', 'Sri Lanka', 2],
-    2: ["Kangaroos Are The National \nAnimal Of What Country?", 'USA', 'India', 'Italy', 'Australia', 'Australia', 4],
-    3: ["What is the capital city \nof Japan?", 'Seoul', 'Beijing', 'Tokyo', 'Bangkok', 'Tokyo', 3],
-    4: ["What is the smallest country \nin the world?", 'Monaco', 'Vatican City', 'San Marino', 'Liechtenstein', 'Vatican City', 2],
-    5: ["What is the longest river \nin the world?", 'Amazon', 'Nile', 'Yangtze', 'Mississippi', 'Nile', 2],
-    6: ["What is the tallest mountain \nin the world?", 'Mount Ruapehu', 'Kangchenjunga', 'Mount Everest', 'Lhotse', 'Mount Everest', 3],
-    7: ["Which country is known \nfor the Taj Mahal?", 'India', 'Pakistan', 'Bangladesh', 'Nepal', 'India', 1],
-    8: ["What is the official language \nof Brazil?", 'Spanish', 'Portuguese', 'French', 'English', 'Portuguese', 2],
-    9: ["What is the capital \nof Canada?", 'Toronto', 'Ottawa', 'Vancouver', 'Montreal', 'Ottawa', 2],
-    10: ["Which country has the largest \npopulation?", 'India', 'USA', 'China', 'Russia', 'China', 3]
-}
 
 # Main class
 class Main:
+
+    # Dictionary of all my questions and answers
+
+    # index 0 is the question, index 1-4 are the answers, index 5 is the correct answer written again, index 6 is the correct answer index
+    q_a ={
+        1: ["The Lion is the National Animal \nof what Country?", 'Nepal', 'Sri Lanka', 'China', 'South Korea', 'Sri Lanka', 2],
+        2: ["Kangaroos Are The National \nAnimal Of What Country?", 'USA', 'India', 'Italy', 'Australia', 'Australia', 4],
+        3: ["What is the capital city \nof Japan?", 'Seoul', 'Beijing', 'Tokyo', 'Bangkok', 'Tokyo', 3],
+        4: ["What is the smallest country \nin the world?", 'Monaco', 'Vatican City', 'San Marino', 'Liechtenstein', 'Vatican City', 2],
+        5: ["What is the longest river \nin the world?", 'Amazon', 'Nile', 'Yangtze', 'Mississippi', 'Nile', 2],
+        6: ["What is the tallest mountain \nin the world?", 'Mount Ruapehu', 'Kangchenjunga', 'Mount Everest', 'Lhotse', 'Mount Everest', 3],
+        7: ["Which country is known \nfor the Taj Mahal?", 'India', 'Pakistan', 'Bangladesh', 'Nepal', 'India', 1],
+        8: ["What is the official language \nof Brazil?", 'Spanish', 'Portuguese', 'French', 'English', 'Portuguese', 2],
+        9: ["What is the capital \nof Canada?", 'Toronto', 'Ottawa', 'Vancouver', 'Montreal', 'Ottawa', 2],
+        10: ["Which country has the largest \npopulation?", 'India', 'USA', 'China', 'Russia', 'China', 3]
+    }
+    
     def __init__(self, parent):
         self.bgcolor = "lightgrey"
         self.textcolor = "white"
@@ -112,7 +114,7 @@ class Question:
         self.randomer()
 
         # Label widget for heading
-        self.question_label = Label(self.quiz_frame, text=q_a[self.qnum][0], font=("impact", 30, "bold"), bg=self.buttoncolor, fg=self.textcolor, relief="solid", bd=5,)
+        self.question_label = Label(self.quiz_frame, text=Main.q_a[self.qnum][0], font=("impact", 30, "bold"), bg=self.buttoncolor, fg=self.textcolor, relief="solid", bd=5,)
         
         self.question_label.pack(pady=30)
 
@@ -121,22 +123,22 @@ class Question:
         self.var1 = IntVar()
 
         # First radio button
-        self.rb1 = Radiobutton(self.quiz_frame, text=q_a[self.qnum][1], font=("impact", 12), bg=self.buttoncolor,fg=self.btextcolor, relief="solid", bd=3, value=1, variable=self.var1, pady=10, padx=10)
+        self.rb1 = Radiobutton(self.quiz_frame, text=Main.q_a[self.qnum][1], font=("impact", 12), bg=self.buttoncolor,fg=self.btextcolor, relief="solid", bd=3, value=1, variable=self.var1, pady=10, padx=10)
         
         self.rb1.pack(pady=10)
 
         # Second radio button
-        self.rb2 = Radiobutton(self.quiz_frame, text=q_a[self.qnum][2], font=("impact", 12), bg=self.buttoncolor, fg=self.btextcolor,relief="solid", bd=3, value=2, variable=self.var1, pady=10, padx=10)
+        self.rb2 = Radiobutton(self.quiz_frame, text=Main.q_a[self.qnum][2], font=("impact", 12), bg=self.buttoncolor, fg=self.btextcolor,relief="solid", bd=3, value=2, variable=self.var1, pady=10, padx=10)
         
         self.rb2.pack(pady=10)
 
         # Third radio button
-        self.rb3 = Radiobutton(self.quiz_frame, text=q_a[self.qnum][3], font=("impact", 12), bg=self.buttoncolor, fg= self.btextcolor,relief="solid", bd=3, value=3, variable=self.var1, pady=10, padx=10)
+        self.rb3 = Radiobutton(self.quiz_frame, text=Main.q_a[self.qnum][3], font=("impact", 12), bg=self.buttoncolor, fg= self.btextcolor,relief="solid", bd=3, value=3, variable=self.var1, pady=10, padx=10)
         
         self.rb3.pack(pady=10)
 
         # Fourth radio button
-        self.rb4 = Radiobutton(self.quiz_frame, text=q_a[self.qnum][4], font=("impact", 12), bg=self.buttoncolor, fg= self.btextcolor,relief="solid", bd=3, value=4, variable=self.var1, pady=10, padx=10)
+        self.rb4 = Radiobutton(self.quiz_frame, text=Main.q_a[self.qnum][4], font=("impact", 12), bg=self.buttoncolor, fg= self.btextcolor,relief="solid", bd=3, value=4, variable=self.var1, pady=10, padx=10)
         
         self.rb4.pack(pady=10)
 
@@ -152,21 +154,28 @@ class Question:
         self.score_label.pack(pady=10)
 
     def randomer(self):
-        self.qnum = random.randint(1, 10)
-        if self.qnum not in self.asked:
-            self.asked.append(self.qnum)
+
+        #this varible contains all questions from the dictionary that is not yet asked 
+        remaining_questions = [q for q in Main.q_a.keys() if q not in self.asked]
+
+        #this shuffles the reamining questions so that the order is ramdom 
+        if remaining_questions:
+            random.shuffle(remaining_questions)
+            self.qnum = remaining_questions[0]
+            self.asked.append(self.qnum) #adds new question to the asked list
+
         else:
-            self.randomer()
+            self.qnum = None     #after all questions have been asked 
 
     def question_setup(self):
         if len(self.asked) < 10:
             self.randomer()
             self.var1.set(0)
-            self.question_label.config(text=q_a[self.qnum][0])
-            self.rb1.config(text=q_a[self.qnum][1])
-            self.rb2.config(text=q_a[self.qnum][2])
-            self.rb3.config(text=q_a[self.qnum][3])
-            self.rb4.config(text=q_a[self.qnum][4])
+            self.question_label.config(text=Main.q_a[self.qnum][0])
+            self.rb1.config(text=Main.q_a[self.qnum][1])
+            self.rb2.config(text=Main.q_a[self.qnum][2])
+            self.rb3.config(text=Main.q_a[self.qnum][3])
+            self.rb4.config(text=Main.q_a[self.qnum][4])
         else:
             self.quiz_frame.destroy()
             End(root, self.score, self.names[0])
@@ -174,11 +183,11 @@ class Question:
     def score_nextq(self):
         choice = self.var1.get()  # This collects the answer the user has chosen
         # This checks if the answer the user has chosen is correct
-        if choice == q_a[self.qnum][6]:
+        if choice == Main.q_a[self.qnum][6]:
             self.score += 1
             self.score_label.configure(text=f"Score: {self.score}")  # This if statement shows what happens when the user gets the question correct, an f string is used for the score label to show the score
         else:
-            self.score_label.configure(text=f"The correct answer was: {q_a[self.qnum][5]}")  # This else statement shows what happens when the user gets the answer wrong, It configures the score label to show the correct answer
+            self.score_label.configure(text=f"The correct answer was: {Main.q_a[self.qnum][5]}")  # This else statement shows what happens when the user gets the answer wrong, It configures the score label to show the correct answer
 
         if len(self.asked) >= 10:
             self.confirm.config(text="Finish", command=self.end_quiz)
